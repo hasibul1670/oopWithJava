a079bc355987c71ad4af6a296d610dbc20698ec4
Hasibul Islam <hasibulislam1670@gmail.com>
2024-02-26 23:30:06 +0600

    learn :: Constructor in Java

---
+++ Encapsulation.java
@@ -0,0 +1,15 @@
+// making Encapsulation 2 ways
+// 01. By grouping a class
+// 02. protection from other by Access modifier
+// access modifier
+// public --> can access
+// private --> access only inside class
+// protected -->
+// internal Protected-->
+public class Encapsulation {
+
+  public static void main(String[] args) {
+    System.out.println("Encapsulation in Java");
+    
+  }
+}
